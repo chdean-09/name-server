8c8c2db998c600e77950f731fe35a7f25a23648b
chdean-09 <chadandrada20@gmail.com>
2025-07-05 21:58:11 +0800

    websocket for backend

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DeviceGateway } from './device/device.gateway';
+import { DeviceModule } from './device/device.module';
 
 @Module({
-  imports: [],
+  imports: [DeviceModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, DeviceGateway],
 })
 export class AppModule {}
