f950edfd5156cbef38a829d456f6bcb09f2de1d1
chdean-09 <chadandrada20@gmail.com>
2025-07-06 00:48:55 +0800

    remove gateway module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { DeviceGateway } from './device/device.gateway';
 import { DeviceModule } from './device/device.module';
 
 @Module({
   imports: [DeviceModule],
   controllers: [AppController],
-  providers: [AppService, DeviceGateway],
+  providers: [AppService],
 })
 export class AppModule {}
