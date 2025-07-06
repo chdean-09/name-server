88e61e30b3d57b146349b3c8c83cc9a28cdd5f69
chdean-09 <chadandrada20@gmail.com>
2025-07-07 01:00:43 +0800

    fix deployment

--- src/device-list/device-list.module.ts
+++ src/device-list/device-list.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { DeviceListService } from './device-list.service';
 import { DeviceListController } from './device-list.controller';
+import { PrismaModule } from 'src/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [DeviceListController],
   providers: [DeviceListService],
 })
