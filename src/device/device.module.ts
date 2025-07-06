63581b08391579ec13c376bb5e617b585396df59
chdean-09 <chadandrada20@gmail.com>
2025-07-07 02:49:24 +0800

    final fix

--- src/device/device.module.ts
+++ src/device/device.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 import { DeviceGateway } from './device.gateway';
-import { DeviceListService } from 'src/device-list/device-list.service';
-import { PrismaService } from 'src/prisma.service';
 import { DeviceListModule } from 'src/device-list/device-list.module';
 
 @Module({
   imports: [DeviceListModule],
-  providers: [DeviceGateway, DeviceListService, PrismaService],
+  providers: [DeviceGateway],
   exports: [DeviceGateway],
 })
 export class DeviceModule {}
